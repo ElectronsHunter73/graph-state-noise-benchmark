a8bf2385f10b2c7058f868fdc599e59112aab33a
electronsHunter73 <ashraf.haitham80@gmail.com>
2026-05-30 20:19:26 +0200

    Gates

--- Test.py
+++
@@ -1,6 +0,0 @@
-from src.graph_noise.states import zero_state, one_state, plus_state, computational_basis_state
-
-print(zero_state())
-print(one_state())
-print(plus_state())
-print(computational_basis_state("01"))
---
+++ src/graph_noise/operators.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+def identity():
+
+    return np.array([[1, 0], [0, 1]], dtype=complex)
+
+def pauli_x():
+    return np.array([[0, 1], [1, 0]], dtype=complex)
+
+def pauli_y():
+    return np.array([[0, -1j], [1j, 0]], dtype=complex)
+
+def pauli_z():
+    return np.array([[1, 0], [0, -1]], dtype=complex)
+
+def hadamard():
+    return (1/np.sqrt(2)) * np.array([[1, 1], [1, -1]], dtype=complex)
+
+def cz():
+    return np.array([[1, 0, 0, 0],
+                     [0, 1, 0, 0],
+                     [0, 0, 1, 0],
+                     [0, 0, 0, -1]], dtype=complex)
+
+def tensor_product(*operators):
+    if len(operators) == 0:
+        raise ValueError("At least one operator must be provided.")
+    result = operators[0]
+    for operator in operators[1:]:
+        result = np.kron(result, operator)
+    return result
